e70654d8644d0134cf2078f341b63acedeb561f1
Rullian Rong <naillur.gnor@gmail.com>
2022-12-14 02:33:20 +0800

    Setup root paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_scope :user do
+    authenticated :user do
+      root to: 'posts#index', as: :authenticated_root
+    end
+  
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resource :posts
 end
