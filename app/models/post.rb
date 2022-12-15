c3d2bdcbc9da5d1f03602eddb2a2a2d6743cfe31
Rullian Rong <naillur.gnor@gmail.com>
2022-12-15 20:53:14 +0800

    Update post controller and views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :pet_name, presence: true
+  validates :category, presence: true
+  validates :breed, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 end
