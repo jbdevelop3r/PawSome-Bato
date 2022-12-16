67d7aaf0cf75ed5bbd6f3134b1bffbbdc69bd6c2
jbdevelop3r <justinebanogon14@gmail.com>
2022-12-16 22:12:55 +0800

    if applied, google maps search feature will be added to the master

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_180755) do
+ActiveRecord::Schema.define(version: 2022_12_16_140149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "pet_name"
@@ -26,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_12_12_180755) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "latitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
