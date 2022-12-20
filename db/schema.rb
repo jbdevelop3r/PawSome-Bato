da8a1594bf1721225b68cd8a50e800dac29943d8
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-12-20 21:32:27 +0800

    add user controller and also views draft
    If applied, this commit will implement users and will also add functionality to check all the user's post in their profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_16_120219) do
+ActiveRecord::Schema.define(version: 2022_12_16_121421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,21 @@ ActiveRecord::Schema.define(version: 2022_12_16_120219) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.string "pet_name"
+    t.string "category"
+    t.string "breed"
+    t.float "price"
+    t.text "description"
+    t.boolean "is_meet_up"
+    t.text "location"
+    t.integer "seller_id"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,11 +61,12 @@ ActiveRecord::Schema.define(version: 2022_12_16_120219) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "users"
   add_foreign_key "reviews", "users"
+  add_foreign_key "transactions", "users"
 end
