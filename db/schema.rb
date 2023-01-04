604e285e7c080f27d5865f4d886bb8d4f27cee48
Rullian Rong <naillur.gnor@gmail.com>
2023-01-04 20:01:19 +0800

    Add sold attribute to post and change index to show only not sold posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_02_051337) do
+ActiveRecord::Schema.define(version: 2023_01_04_115701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2023_01_02_051337) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sold"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
