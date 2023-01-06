9b2b722eff7a921018cd6b862028430e9c8e0095
jbdevelop3r <justinebanogon14@gmail.com>
2023-01-06 21:02:22 +0800

    rullian post copy

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_01_02_051337) do
-
+ActiveRecord::Schema.define(version: 2023_01_06_121340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +34,9 @@ ActiveRecord::Schema.define(version: 2023_01_02_051337) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "latitude"
     t.string "thumbnail"
     t.string "availability", default: "available"
-    t.decimal "latitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
