ce22bfc048216bafffb7a213343d0fa25392b291
Rullian Rong <naillur.gnor@gmail.com>
2023-01-07 12:43:22 +0800

    Apply design to post index, new, and show views

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -22,8 +22,8 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
+  process scale: [200, 300]
+  
   # def scale(width, height)
   #   # do something
   # end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_01_04_124908) do
-
+ActiveRecord::Schema.define(version: 2023_01_06_121340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "connections", force: :cascade do |t|
     t.bigint "customer_id"
@@ -24,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_01_04_124908) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_connections_on_user_id"
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
@@ -31,7 +29,6 @@ ActiveRecord::Schema.define(version: 2023_01_04_124908) do
     t.decimal "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,10 +42,10 @@ ActiveRecord::Schema.define(version: 2023_01_04_124908) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "images"
     t.string "thumbnail"
     t.string "availability", default: "available"
     t.boolean "is_sold"
-    t.decimal "latitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
