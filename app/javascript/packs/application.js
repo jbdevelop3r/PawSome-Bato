92a428f1aab8b5b3e30e8c0484050b23fa83cbdf
MisterScrooge <shawnhumbert13@gmail.com>
2023-01-07 12:37:21 +0800

    removed some functions application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,9 +14,9 @@ ActiveStorage.start()
 
 import "controllers"
 
-window.dispatchMapsEvent = function (...args) {
-    const event = document.createEvent("Events")
-    event.initEvent("google-maps-callback", true, true)
-    event.args = args
-    window.dispatchEvent(event)
-  }
+// window.dispatchMapsEvent = function (...args) {
+//     const event = document.createEvent("Events")
+//     event.initEvent("google-maps-callback", true, true)
+//     event.args = args
+//     window.dispatchEvent(event)
+//   }
