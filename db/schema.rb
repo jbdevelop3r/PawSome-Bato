0af450b5504f8d7b1e3f700267b1ef7780cc6f67
Vince <103049779+vincent-larisma@users.noreply.github.com>
2023-01-08 13:31:32 +0800

    User profile design implemented
    User profile added changed of user image using carrierwave gem and also added the final design of the user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_06_121340) do
+ActiveRecord::Schema.define(version: 2023_01_08_045053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,9 @@ ActiveRecord::Schema.define(version: 2023_01_06_121340) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "latitude"
+    t.boolean "is_sold"
     t.string "thumbnail"
     t.string "availability", default: "available"
-    t.boolean "is_sold"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -68,6 +67,7 @@ ActiveRecord::Schema.define(version: 2023_01_06_121340) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
