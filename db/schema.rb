c68f8c26054356d29f73d4768efaa78df8ee7773
MisterScrooge <shawnhumbert13@gmail.com>
2023-01-09 20:05:13 +0800

    schema update upon testing

--- db/schema.rb
+++ db/schema.rb
@@ -41,10 +41,9 @@ ActiveRecord::Schema.define(version: 2023_01_08_234929) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "latitude"
+    t.boolean "is_sold"
     t.string "thumbnail"
     t.string "availability", default: "available"
-    t.boolean "is_sold"
     t.string "pick_up"
     t.string "advertisement"
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -68,8 +67,7 @@ ActiveRecord::Schema.define(version: 2023_01_08_234929) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false
-    t.string "avatar"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
