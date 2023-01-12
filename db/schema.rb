8fd13a9db59e3eb2793d048b83a33c6a9b7f13ba
Vince <103049779+vincent-larisma@users.noreply.github.com>
2023-01-12 22:40:29 +0800

    Add Post implement design, Header, Footer

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2023_01_10_012932) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
